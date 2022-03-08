e30423b4b77f9052c4e96ccf57f688789e70fe94
JavierLainez420 <javierlainez123@gmail.com>
2022-03-08 11:44:11 -0600

    ValorMoneda

--- src/miprimerappjava/Ejercicios/ValorMonedaPaises.java
+++ src/miprimerappjava/Ejercicios/ValorMonedaPaises.java
@@ -10,5 +10,24 @@ package miprimerappjava.Ejercicios;
  * @author ITCA
  */
 public class ValorMonedaPaises {
-    
+    public static void main(String[] args) {
+        double monedaEuropa, monedaReinoUnido, monedaAustralia, monedaCanada,dolares;
+        monedaEuropa = 0.70;
+        monedaReinoUnido = 0.61;
+        monedaAustralia = 0.95;
+        monedaCanada = 0.97;
+        dolares = 100.00;
+        
+        monedaEuropa = monedaEuropa * dolares;
+        monedaReinoUnido = monedaReinoUnido * dolares;
+        monedaAustralia = monedaAustralia * dolares;
+        monedaCanada = monedaCanada * dolares;
+        
+        System.out.println("Conversion de Moneda de Europa: " + monedaEuropa);
+        System.out.println("Conversion de Moneda de Reino Unido: " + monedaReinoUnido);
+        System.out.println("Conversion de Moneda de Australia: " + monedaAustralia);
+        System.out.println("Conversion de Moneda de Canada: " + monedaCanada);
+        
+        
+    }
 }
